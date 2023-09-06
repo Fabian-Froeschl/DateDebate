5c9a2b5b6437a039a4c507cd863aa3c1354535cf
leonpoiss <l.poiss@htlkrems.at>
2023-09-06 11:33:40 +0200

    base

--- lemp/app/index.php
+++ lemp/app/index.php
@@ -1,39 +1,40 @@
-<?php
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DateDebate</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity ="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <ul class="nav">
+        <li class="nav-item">
+            <h1>DateDebate</h1>
+        </li>
+    </ul>
+    <div class="container">
 
-$magicNumbers = [
-    rand(0, 100),
-    rand(100, 200),
-    rand(0, 1000),
-];
-
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta name="author" content="Steven Liebregt" />
-        <title>Hello, world!</title>
-        <style>
-            table, th, td {
-                border: 1px solid black;
-                border-collapse: collapse;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Hello, world!</h1>
-        <p>Your magic numbers are:</p>
-        <table>
-            <tr>
-                <th>#</th>
-                <th>Number</th>
-            <tr>
-            <?php foreach ($magicNumbers as $index => $number): ?>
-                <tr>
-                    <td><?php echo $index; ?></td>
-                    <td><?php echo $number; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        <p>If you can see this properly, then it means PHP is working fine.</p>
-    </body>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Event1</h5>
+                    <h5 class="card-text">Jahreszahl</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+            <div class="col-sm">
+                One of three columns
+            </div>
+            <div class="col-sm">
+                One of three columns
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
 </html>
