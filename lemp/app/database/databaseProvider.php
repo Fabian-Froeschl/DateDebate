5f6642a98329b57547747bd8d96d2716d59f5901
j-gruenstaeudl <jakob.tobias.g@gmail.com>
2023-09-06 21:34:33 +0200

    lemp is maybe working

--- lemp/app/database/databaseProvider.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = "localhost";
-$port = "3306";
-$database = "datedebate";
-$username = "root";
-$password = "";
-
-$connection = mysqli_connect($hostname, $username, $password, $database, $port);
