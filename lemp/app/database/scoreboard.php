ea1503eb435708e32ccce528e777b60b719bb6c1
IIITobiasIII <t.fraberger@htlkrems.at>
2023-09-07 11:06:37 +0200

    Updated connections

--- lemp/app/database/scoreboard.php
+++ lemp/app/database/scoreboard.php
@@ -5,12 +5,6 @@ include 'database.php';
 
 function getPlayerByRank($rank){
     global $scoreboard;
-    $key_values = array_column($scoreboard, 'score');
-    array_multisort($key_values, SORT_DESC, $scoreboard);
     return $scoreboard[$rank];
 }
 
-#$result = getPlayerByRank(0);
-
-#echo "{$result['name']}------{$result['score']}";
-#echo $scoreboard[1]['score'];
