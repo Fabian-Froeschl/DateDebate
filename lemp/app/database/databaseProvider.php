d2865a7f794028f6a3de765e067c3038678909e9
j-gruenstaeudl <jakob.tobias.g@gmail.com>
2023-09-07 08:08:07 +0200

    docker is working

---
+++ lemp/app/database/databaseProvider.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = "172.21.0.2";
+$port = "3306";
+$database = "datedebate";
+$username = "root";
+$password = "!krems";
+
+$connection = mysqli_connect($hostname, $username, $password, $database, $port);
+
