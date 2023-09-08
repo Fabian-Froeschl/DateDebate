fcc76c54534c405c21c8142699def3c30a34bc4d
Kevin Fichtenbauer <131268253+kevinfichtenbauer@users.noreply.github.com>
2023-09-08 08:58:19 +0200

    scoreboard fix

--- lemp/app/scoreboard.php
+++ lemp/app/scoreboard.php
@@ -5,7 +5,6 @@ ini_set('display_errors', 0);
 <!doctype html>
 <html lang="de">
 <head>
-@ -5,27 +9,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -15,11 +14,7 @@ ini_set('display_errors', 0);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 <body>
-    <h1>Scoreboard</h1>
-    <input id="userInputName" type="text">
-    <button id="userInputSubmit">Submit!</button>
-
-    <table id="scoreboardTable">
+    
     <h3 class="text-center">Scoreboard</h3>
     <div class="text-center">
         <input id="userInputName" type="text">
@@ -38,7 +33,6 @@ ini_set('display_errors', 0);
         ?>
     </table>
     <button id="retry">Retry</button>
-        <button id="retry">Retry</button>
     
     <script src="js/scoreboard.js"></script>
 </body>
