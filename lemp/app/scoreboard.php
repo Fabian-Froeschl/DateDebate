97dcccd097c405f58fe465afc28d572d066d25f8
Kevin Fichtenbauer <131268253+kevinfichtenbauer@users.noreply.github.com>
2023-09-08 08:56:55 +0200

    updated scoreboard design

--- lemp/app/scoreboard.php
+++ lemp/app/scoreboard.php
@@ -1,10 +1,16 @@
+<?php
+ini_set('error_reporting', 0);
+ini_set('display_errors', 0);
+?>
 <!doctype html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
+@ -5,27 +9,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Scoreboard</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
@@ -14,6 +20,12 @@
     <button id="userInputSubmit">Submit!</button>
 
     <table id="scoreboardTable">
+    <h3 class="text-center">Scoreboard</h3>
+    <div class="text-center">
+        <input id="userInputName" type="text">
+        <button id="userInputSubmit">Submit!</button>
+    </div>
+    <table id="scoreboardTable" class="table table-striped table-responsive table-bordered" style="width: 70%; margin:auto; margin-top: 2%;">
         <?php
 
         global $scoreboard;
@@ -26,6 +38,8 @@
         ?>
     </table>
     <button id="retry">Retry</button>
+        <button id="retry">Retry</button>
+    
     <script src="js/scoreboard.js"></script>
 </body>
 </html>
