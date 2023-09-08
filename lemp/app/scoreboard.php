c7ab3d87fc0a185052f31048b7ea347aebdb55ec
Kevin Fichtenbauer <131268253+kevinfichtenbauer@users.noreply.github.com>
2023-09-08 09:09:44 +0200

    updated scoreboard design

--- lemp/app/scoreboard.php
+++ lemp/app/scoreboard.php
@@ -10,6 +10,7 @@ ini_set('display_errors', 0);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+          <link rel="stylesheet" href="css/styleGame.css">
     <title>Scoreboard</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
@@ -18,7 +19,7 @@ ini_set('display_errors', 0);
     <h3 class="text-center">Scoreboard</h3>
     <div class="text-center">
         <input id="userInputName" type="text">
-        <button id="userInputSubmit">Submit!</button>
+        <button id="userInputSubmit" class="btn btn-light btn1">Submit!</button>
     </div>
     <table id="scoreboardTable" class="table table-striped table-responsive table-bordered" style="width: 70%; margin:auto; margin-top: 2%;">
         <?php
@@ -32,8 +33,9 @@ ini_set('display_errors', 0);
 
         ?>
     </table>
-    <button id="retry">Retry</button>
-    
+    <div style="text-align:center; margin-top: 20px;">
+        <button id="retry" class="btn btn-light btn1">Retry</button>
+    </div>
     <script src="js/scoreboard.js"></script>
 </body>
 </html>
